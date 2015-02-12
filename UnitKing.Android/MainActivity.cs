a4d0fbb9e9042f3fea07a4f489df63cdeb8c6082
Guy Micciche <king.guy@outlook.com>
2015-02-12 04:21:23 -0500

    Initial commit

---
+++ UnitKing.Android/MainActivity.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using UnitKing.Core;
+using Android.Views.InputMethods;
+
+namespace UnitKing.Android
+{
+    [Activity(Label = "UnitKing.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button add;
+        private EditText current;
+        private EditText target;
+        private EditText carbohydrates;
+        private TextView suggested;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.MainActivity);
+            
+            InitUI();
+            AddEventHandlers();
+            Calculation.Weight = 195;
+        }
+
+        private void AddEventHandlers()
+        {
+            current.TextChanged += current_TextChanged;
+            target.TextChanged += target_TextChanged;
+            carbohydrates.TextChanged += carbohydrates_TextChanged;
+
+            current.FocusChange += view_FocusChange;
+            target.FocusChange += view_FocusChange;
+            carbohydrates.FocusChange += view_FocusChange;
+        }
+
+        void view_FocusChange(object sender, View.FocusChangeEventArgs e)
+        {
+            if(!e.HasFocus)
+            {
+                InputMethodManager manager = (InputMethodManager)GetSystemService(InputMethodService);
+                manager.HideSoftInputFromWindow(((View)sender).WindowToken, HideSoftInputFlags.NotAlways);
+            }
+        }
+
+
+        void current_TextChanged(object sender, global::Android.Text.TextChangedEventArgs e)
+        {
+            int num = 0;
+            if (int.TryParse(e.Text.ToString(), out num))
+            {
+                Calculation.Actual = num;
+            }
+
+            DisplaySuggestedUnits();
+        }
+
+        void target_TextChanged(object sender, global::Android.Text.TextChangedEventArgs e)
+        {
+            int num = 0;
+            if (int.TryParse(e.Text.ToString(), out num))
+            {
+                Calculation.Target = num;
+            }
+
+            DisplaySuggestedUnits();
+        }
+
+        void carbohydrates_TextChanged(object sender, global::Android.Text.TextChangedEventArgs e)
+        {
+            int num = 0;
+            if (int.TryParse(e.Text.ToString(), out num))
+            {
+                Calculation.Carbohydrates = num;
+            }
+
+            DisplaySuggestedUnits();
+        }
+
+        private void DisplaySuggestedUnits()
+        {
+            suggested.Text = Calculation.TotalMealtimeDose + " units";
+        }
+
+        private void InitUI()
+        {
+            add = FindViewById<Button>(Resource.Id.saveBtn);
+            current = FindViewById<EditText>(Resource.Id.current);
+            target = FindViewById<EditText>(Resource.Id.target);
+            carbohydrates = FindViewById<EditText>(Resource.Id.carbohydrates);
+            suggested = FindViewById<TextView>(Resource.Id.suggested);
+        }
+    }
+}
+
---
+++ UnitKing.Core/UnitCalculations.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitKing.Core
+{
+    public class Calculation
+    {
+        public static int Weight { get; set; }
+        public static int Actual { get; set; }
+        public static int Target { get; set; }
+        public static int Carbohydrates { get; set; }
+        public static bool IsPouunds { get; set; }
+
+        public static void InitCalculation(int weight, bool isPounds = true)
+        {
+            Weight = weight;
+            IsPouunds = isPounds;
+        }
+
+        public static void InitCalculation(int weight, int actual, int target, int carbohydrates, bool isPounds = true)
+        {
+            Weight = weight;
+            Actual = actual;
+            Target = target;
+            Carbohydrates = carbohydrates;
+            IsPouunds = isPounds;
+        }
+
+        public static int TotalDailyInsulinRequirement
+        {
+            get
+            {
+			    double units = 0;
+
+                if (IsPouunds)
+                {
+                    units = Weight / 4;
+			    }
+			    else
+                {
+                    units = Weight * .55;
+			    }
+
+			    return (int)Math.Round(units);
+            }
+		}
+
+		public static int CarbohydrateCoverageRatio
+        {
+            get
+            {
+			    double ccr = 500 / TotalDailyInsulinRequirement;
+
+                return (int)Math.Round(ccr);
+            }
+		}
+
+		public static int CorrectionFactor
+        {
+            get
+            {
+                double cf = 1800 / TotalDailyInsulinRequirement;
+
+			    return (int)Math.Round(cf);
+            }
+		}
+
+		public static int CorrectionDose
+        {
+            get
+            {
+                double cd = (Actual - Target) / CorrectionFactor;
+
+			    return (int)Math.Round(cd);
+            }
+		}
+
+		public static int MealtimeDose
+        {
+            get
+            {
+                double md = Carbohydrates / CarbohydrateCoverageRatio;
+
+			    return (int)Math.Round(md);
+            }
+		}
+
+		public static int TotalMealtimeDose
+        {
+            get
+            {
+                int tmd = CorrectionDose + MealtimeDose;
+
+			    return tmd;
+            }
+		}
+    }
+}
